f3f76e95797af452036137f71df5d591ce6cc423
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2025-03-04 13:57:43 +0100

    Update search.py

--- search.py
+++ search.py
@@ -8,5 +8,5 @@ for loop in fingerprints:
     command = "sudo python3 app.py "+sys.argv[1]+" "+loop
     exit_code = os.system(command)
     if exit_code == 0:
-        print("Fingerprint matched. Stopping the loop.")
+        print("Fingerprint matched. Stopping the Search.")
         break
