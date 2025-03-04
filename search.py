14fac5d7e204a07b8ade322faa269b05a27aba73
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2025-03-04 13:38:11 +0100

    Update search.py

--- search.py
+++ search.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-x= "database"
-fingerprints = os.listdir(x)
+db= "database"
+fingerprints = os.listdir(db)
 
 for loop in fingerprints:
-    command = "sudo python3 app.py "+sys.argv[1]+" "+loop
+    command = "sudo python3 recognition_model/app.py "+sys.argv[1]+" "+loop
     os.system(command)
 
